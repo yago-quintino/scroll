88300ea9313f8caf10d8eac467866d5e2fafb175
yago-quintino <yagoquintino21@gmail.com>
2022-10-31 14:06:23 -0300

    react-query pagination

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import Characters from "./Characters";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <h1>Rick and Morty</h1>
+        <Characters />
+      </div>
+    </div>
+  );
+}
+
+export default App;
